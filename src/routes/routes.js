335275a8a951c44e57890d17b2b3ca6b746870dd
Julian Wildmoser <julian.wildmoser@tum.de>
2025-06-22 21:34:30 +0200

    Added routes for add-delete-edit functionality and changed it to make it more modular

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,26 @@
 
 const express = require('express');
 const router = express.Router(); // Create a new Express Router instance
+const Item = require('../models/item'); // Import the Item model
+const multer = require('multer'); // Import multer for handling file uploads
+const storage = multer.memoryStorage(); // Use memory storage for file uploads
+const upload = multer({ storage: storage, limits: { files: 3 } }); // Create a multer instance with the memory storage
+const { getAllItems, createItem, deleteItem, updateItem, getItemById, getItemImage} = require('../controllers/item.controller');
 
+// Get route to fetch all items using the controller function
+router.get('/items', getAllItems);
 
-router.get('/', (req, res) => {
-  res.send('Hello from the Backend!');
-});
+// Post route to handle item creation using the controller function
+router.post('/items', upload.array('images', 3), createItem);
 
-router.get('/test', (req, res) => {
-  res.json({ message: 'This is a test route to the Backend!' });
-});
+// Delete route to handle item deletion by ID using the controller function
+router.delete('/items/:id', deleteItem);
 
+// Put route to handle item updates by ID using the controller function
+router.put('/items/:id', updateItem);
+
+router.get('/items/:id/image/:index', getItemImage);
+
+router.get('/items/:id', getItemById);
 
 module.exports = router;
