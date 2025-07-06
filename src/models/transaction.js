fd85c221e62c06c8006a958ac35f4d16ea15a95f
Julian Wildmoser <julian.wildmoser@tum.de>
2025-07-06 11:34:39 +0200

    added returnDate to transaction model

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -6,6 +6,7 @@ const transactionSchema = new mongoose.Schema({
   borrower:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
   status:    { type: String, enum: ['requested', 'accepted', 'rejected', 'borrowed', 'returned', 'completed'], default: 'requested' },
   requestDate: { type: Date, default: Date.now },
+  returnDate: { type: Date},
   requestedFrom: { type: Date, required: true },
   requestedTo:   { type: Date, required: true },
   lenderReviewed: { type: Boolean, default: false },
