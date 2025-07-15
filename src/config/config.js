aca2d396e94f03cbc4d29aa992be67641efe3dde
Jacob Fehn <ge75xos@mytum.de>
2025-07-15 11:31:00 +0200

    fixed the double paypal_client in config.js and tested the functionality of config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export default {
   PORT: 3000,
   MONGO_URI: "mongodb+srv://SeBaAdmin:SeBa2025@localloop.7ty0q8l.mongodb.net/?retryWrites=true&w=majority&appName=LocalLoop",
-  PAYPAL_CLIENT_ID: "PAYPAL_CLIENT_ID=AXP0w_XIqcSbu3OdhrghpzVSIDbxSDiWHSXEc7leSJmnEeqD11GJYP3swV_ppwaxcYcJzTCN7gIN3xFc",
+  PAYPAL_CLIENT_ID: "AXP0w_XIqcSbu3OdhrghpzVSIDbxSDiWHSXEc7leSJmnEeqD11GJYP3swV_ppwaxcYcJzTCN7gIN3xFc",
   JWT_SECRET: "aSuperLongRandomString"
 };
 
