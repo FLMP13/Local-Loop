d02c385da28006fce745ee694cdf09f5b26becd0
Nadia Mallik <ga53tev@mytum.de>
2025-07-16 18:12:38 +0200

    deleted .env file and added Paypal config

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,11 @@ export default {
   PORT: 3000,
   MONGO_URI: "mongodb+srv://SeBaAdmin:SeBa2025@localloop.7ty0q8l.mongodb.net/?retryWrites=true&w=majority&appName=LocalLoop",
   PAYPAL_CLIENT_ID: "AXP0w_XIqcSbu3OdhrghpzVSIDbxSDiWHSXEc7leSJmnEeqD11GJYP3swV_ppwaxcYcJzTCN7gIN3xFc",
-  JWT_SECRET: "aSuperLongRandomString"
+  JWT_SECRET: "aSuperLongRandomString",
+  PAYPAL_CLIENT_SECRET: "EDT_s7KAFcuX9V9AQGj8ZcPGhCzxOcbrlrG0qI6aopgbyJK9fxDQRylUvChlRTWrTDaE3ZtWWD6fXW8p",
+  PAYPAL_MODE: "sandbox",
+  PAYPAL_MONTHLY_PLAN_ID: "P-3TT94183MC487024WNB3IRKI",
+  PAYPAL_YEARLY_PLAN_ID: "P-0JY26374DB1999900NB3IRKI"
+
 };
 
